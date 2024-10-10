0b5b85dd93aac284087d8ebaa572015e67363a60
myhql <1051576073@qq.com>
2024-10-10 23:44:32 +0800

    feat 引用对象支持弱引用
    Signed-off-by: myhql <1051576073@qq.com>

--- SimpleTool/RefObject.hpp
+++ SimpleTool/RefObject.hpp
@@ -23,35 +23,6 @@
 
 #include <new>
 
-////引入日志
-//#ifdef SIM_NO_LOGGER
-//#include "Logger.hpp"
-//#else
-////格式
-//#define SIM_FORMAT_NUM(num,base,w,f)	
-//#define SIM_FORMAT_STR(str,w,f)			
-//#define SIM_HEX(num) 
-//#define SIM_FORMAT_STR0(str,w) 
-//
-////防止重名
-//#define SIM_FUNC(lv)
-//#define SIM_FUNC_DEBUG() 
-//
-////新增输出流
-//#define SIM_LOG_ADD(Stream,...) 
-////配置输出句柄
-//#define SIM_LOG_HANDLER(max_lv,handler,userdata) 
-//
-////配置控制台输出
-//#define SIM_LOG_CONSOLE(max_lv)
-//
-//#define SIM_LOG(lv,x)
-//#define SIM_LDEBUG(x) 
-//#define SIM_LINFO(x) 
-//#define SIM_LWARN(x) 
-//#define SIM_LERROR(x)
-//#endif // USING_SIM_LOGGER
-
 namespace sim
 {
 	//引用计数对象
@@ -91,7 +62,40 @@ namespace sim
 			return ref_count_;
 		}
 
-		RefCountable(RefCountType count=1) : ref_count_(count) 
+		RefCountType add_weak_ref(void)
+		{
+#ifdef OS_WINDOWS
+			return ::InterlockedIncrement(&ref_weak_count_);
+#else
+			//linux上面用锁实现
+			pthread_mutex_lock(&lock_);
+			RefCountType t = ++ref_weak_count_;
+			pthread_mutex_unlock(&lock_);
+			return t;
+#endif
+
+		}
+
+		RefCountType dec_weak_ref(void)
+		{
+#ifdef OS_WINDOWS
+			return ::InterlockedDecrement(&ref_weak_count_);
+#else
+			//linux上面用锁实现
+			pthread_mutex_lock(&lock_);
+			RefCountType t = --ref_weak_count_;
+			pthread_mutex_unlock(&lock_);
+			return t;
+
+#endif
+		}
+
+		RefCountType get_ref_weak_count(void) const
+		{
+			return ref_weak_count_;
+		}
+
+		RefCountable(RefCountType count=1) : ref_count_(count), ref_weak_count_(0)
 		{
 #ifdef OS_LINUX
 			pthread_mutex_init(&lock_, NULL);
@@ -104,6 +108,8 @@ namespace sim
 
 	private:
 		RefCountType volatile ref_count_;
+		//弱引用
+		RefCountType volatile ref_weak_count_;
 #ifdef OS_LINUX
 		//linux上面用锁实现
 		pthread_mutex_t lock_;
@@ -114,11 +120,21 @@ namespace sim
 	typedef void(*RefObjectDelete)(void* ptr, void*pdata);
 
 	template <typename T>
+	class RefWeakObject;
+
+	template <typename T>
 	class RefObject
 	{
+		friend class RefWeakObject<T>;
 	public:
 		RefObject(T* p=NULL, RefObjectDelete deleter=NULL, void*pdata=NULL)
 			:ptr_(p), deleter_(deleter), ref_count_ptr_(new RefCountable(1)), pdata_(pdata)
+		{
+			//新增引用
+			//add_ref();
+		}
+		RefObject(RefCountable *ref_count_ptr,T* p, RefObjectDelete deleter, void* pdata)
+			:ptr_(p), deleter_(deleter), ref_count_ptr_(ref_count_ptr), pdata_(pdata)
 		{
 			//新增引用
 			//add_ref();
@@ -193,8 +209,8 @@ namespace sim
 	protected:
 		void release()
 		{
-			//释放
-			if (ref_count_ptr_->dec_ref() <= 0)
+			//释放 强引用和弱引用 都没有之后
+			if (ref_count_ptr_->dec_ref() <= 0&& ref_count_ptr_->get_ref_weak_count()<=0)
 			{
 				if (ptr_)
 				{
@@ -217,6 +233,123 @@ namespace sim
 		void*pdata_;
 		//引用计数指针
 		RefCountable*ref_count_ptr_;
+	};
+
+	template <typename T>
+	class RefWeakObject
+	{
+	public:
+		//// 浅拷贝
+		RefWeakObject(const RefObject<T>& orig)
+			:ptr_(orig.ptr_),
+			deleter_(orig.deleter_),
+			ref_count_ptr_(orig.ref_count_ptr_),
+			pdata_(orig.pdata_)
+		{
+			ref_count_ptr_->add_weak_ref();
+		}
+		//// 浅拷贝
+		RefWeakObject(const RefWeakObject<T>& orig)
+			:ptr_(orig.ptr_),
+			deleter_(orig.deleter_),
+			ref_count_ptr_(orig.ref_count_ptr_),
+			pdata_(orig.pdata_)
+		{
+			ref_count_ptr_->add_weak_ref();
+		}
+		virtual ~RefWeakObject()
+		{
+			//释放
+			release();
+		}
+		
+		//// 浅拷贝
+		virtual RefWeakObject<T>& operator=(const RefObject<T>& rhs)
+		{
+			{
+				rhs.ref_count_ptr_->add_weak_ref();
+
+				//释放
+				release();
+
+				//赋值
+				ptr_ = rhs.ptr_;
+				ref_count_ptr_ = rhs.ref_count_ptr_;
+				deleter_ = rhs.deleter_;
+				pdata_ = rhs.pdata_;
+			}
+			return *this;
+		}
+
+		virtual RefWeakObject<T>& operator=(const RefWeakObject<T>& rhs)
+		{
+			//自己赋值给自己的情况不算
+			if (this != &rhs)
+			{
+				rhs.ref_count_ptr_->add_weak_ref();
+
+				//释放
+				release();
+
+				//赋值
+				ptr_ = rhs.ptr_;
+				ref_count_ptr_ = rhs.ref_count_ptr_;
+				deleter_ = rhs.deleter_;
+				pdata_ = rhs.pdata_;
+			}
+			return *this;
+		}
+
+		virtual RefObject<T> ref_object()
+		{
+			if (ref_count_ptr_->get_ref_count()<=0)
+			{
+				//返回空引用
+				return RefObject<T>();
+			}
+			ref_count_ptr_->add_ref();
+			return RefObject<T>(ref_count_ptr_, ptr_, deleter_, pdata_);
+		}
+
+		virtual RefCountType getcount()
+		{
+			return ref_count_ptr_->get_ref_weak_count();
+		}
+
+		virtual void reset(T* p = NULL)
+		{
+			release();
+			ptr_ = p;
+			ref_count_ptr_ = new RefCountable(0);
+			ref_count_ptr_->add_weak_ref();
+		}
+	protected:
+		void release()
+		{
+			//释放 强引用和弱引用 都没有之后
+			if (ref_count_ptr_->dec_weak_ref() <= 0 && ref_count_ptr_->get_ref_count() <= 0 )
+			{
+				if (ptr_)
+				{
+					if (deleter_)
+						deleter_(ptr_, pdata_);
+					else
+						delete ptr_;
+				}
+				ptr_ = NULL;
+
+				//回收计数指针
+				delete ref_count_ptr_;
+				ref_count_ptr_ = NULL;
+			}
+		}
+	protected:
+		//指针
+		T* ptr_;
+		RefObjectDelete deleter_;
+		void* pdata_;
+		//引用计数指针
+		RefCountable* ref_count_ptr_;
 	};
 
 	//引用缓存
--- test/ref_object.cpp
+++ test/ref_object.cpp
@@ -19,39 +19,39 @@ struct RefObjectTest
 SIM_TEST(RefObjectEmpty)
 {
 	sim::RefObject<int> int_ptr;
-	SIM_TEST_IS_FALSE(int_ptr);
-	SIM_TEST_IS_NULL(int_ptr);
-	SIM_TEST_IS_NULL(int_ptr.get());
-	SIM_TEST_IS_EQUAL(1, int_ptr.getcount());
+	SIM_ASSERT_IS_FALSE(int_ptr);
+	SIM_ASSERT_IS_NULL(int_ptr);
+	SIM_ASSERT_IS_NULL(int_ptr.get());
+	SIM_ASSERT_IS_EQUAL(1, int_ptr.getcount());
 	int_ptr.reset();
-	SIM_TEST_IS_NULL(int_ptr.get());
-	SIM_TEST_IS_EQUAL(1, int_ptr.getcount());
+	SIM_ASSERT_IS_NULL(int_ptr.get());
+	SIM_ASSERT_IS_EQUAL(1, int_ptr.getcount());
 }
 SIM_TEST(RefObjectBuildIn)
 {
 	sim::RefObject<int> int_ptr(new int(0));
-	SIM_TEST_IS_TRUE(int_ptr);
-	SIM_TEST_IS_FALSE(NULL == int_ptr);
-	SIM_TEST_IS_FALSE(NULL == int_ptr.get());
-	SIM_TEST_IS_EQUAL(1, int_ptr.getcount());
-	SIM_TEST_IS_EQUAL(0, *int_ptr);
+	SIM_ASSERT_IS_TRUE(int_ptr);
+	SIM_ASSERT_IS_FALSE(NULL == int_ptr);
+	SIM_ASSERT_IS_FALSE(NULL == int_ptr.get());
+	SIM_ASSERT_IS_EQUAL(1, int_ptr.getcount());
+	SIM_ASSERT_IS_EQUAL(0, *int_ptr);
 
 	sim::RefObject<int> int_ptr2(int_ptr);
-	SIM_TEST_IS_EQUAL(int_ptr.getcount(), int_ptr2.getcount());
-	SIM_TEST_IS_EQUAL(2, int_ptr2.getcount());
-	SIM_TEST_IS_EQUAL(0, *int_ptr2);
+	SIM_ASSERT_IS_EQUAL(int_ptr.getcount(), int_ptr2.getcount());
+	SIM_ASSERT_IS_EQUAL(2, int_ptr2.getcount());
+	SIM_ASSERT_IS_EQUAL(0, *int_ptr2);
 
 	sim::RefObject<int> int_ptr3(new int(3));
 	int_ptr3 = int_ptr;//这里会释放一个
-	SIM_TEST_IS_EQUAL(int_ptr.getcount(), int_ptr3.getcount());
-	SIM_TEST_IS_EQUAL(3, int_ptr3.getcount());
-	SIM_TEST_IS_EQUAL(0, *int_ptr3);
+	SIM_ASSERT_IS_EQUAL(int_ptr.getcount(), int_ptr3.getcount());
+	SIM_ASSERT_IS_EQUAL(3, int_ptr3.getcount());
+	SIM_ASSERT_IS_EQUAL(0, *int_ptr3);
 
 	int_ptr.reset();
-	SIM_TEST_IS_FALSE(int_ptr);
-	SIM_TEST_IS_EQUAL(int_ptr2.getcount(), int_ptr3.getcount());
-	SIM_TEST_IS_EQUAL(2, int_ptr3.getcount());
-	SIM_TEST_IS_EQUAL(0, *int_ptr3);
+	SIM_ASSERT_IS_FALSE(int_ptr);
+	SIM_ASSERT_IS_EQUAL(int_ptr2.getcount(), int_ptr3.getcount());
+	SIM_ASSERT_IS_EQUAL(2, int_ptr3.getcount());
+	SIM_ASSERT_IS_EQUAL(0, *int_ptr3);
 }
 
 SIM_TEST(RefObjectObject)
@@ -64,36 +64,91 @@ SIM_TEST(RefObjectObject)
 		object_ptr2 = object_ptr1;
 		object_ptr1 = object_ptr;
 	}
-	SIM_TEST_IS_EQUAL(0, i);
+	SIM_ASSERT_IS_EQUAL(0, i);
 }
 
 SIM_TEST(RefBuff)
 {
 	sim::RefBuff buff;
-	SIM_TEST_IS_NULL(buff);
-	SIM_TEST_IS_NULL(buff.get());
-	SIM_TEST_IS_EQUAL(0,buff.size());
-	SIM_TEST_IS_EQUAL(1,buff.getcount());
+	SIM_ASSERT_IS_NULL(buff);
+	SIM_ASSERT_IS_NULL(buff.get());
+	SIM_ASSERT_IS_EQUAL(0,buff.size());
+	SIM_ASSERT_IS_EQUAL(1,buff.getcount());
 
 	sim::RefBuff buff1(buff);
-	SIM_TEST_IS_NULL(buff);
-	SIM_TEST_IS_NULL(buff.get());
-	SIM_TEST_IS_EQUAL(0, buff.size());
-	SIM_TEST_IS_EQUAL(2, buff.getcount());
+	SIM_ASSERT_IS_NULL(buff);
+	SIM_ASSERT_IS_NULL(buff.get());
+	SIM_ASSERT_IS_EQUAL(0, buff.size());
+	SIM_ASSERT_IS_EQUAL(2, buff.getcount());
 
 	const char* test1 = "hello world";
 	sim::RefBuff buff2(test1);
-	//SIM_TEST_IS_EQUAL(test1, buff2.get());
-	SIM_TEST_IS_EQUAL(strlen(test1), buff2.size());
+	//SIM_ASSERT_IS_EQUAL(test1, buff2.get());
+	SIM_ASSERT_IS_EQUAL(strlen(test1), buff2.size());
 	for(int i=0;i< buff2.size();++i)
-		SIM_TEST_IS_EQUAL(test1[i], buff2[i]);
+		SIM_ASSERT_IS_EQUAL(test1[i], buff2[i]);
 
 	sim::RefBuff buff3(buff2);
 	buff1 = buff3;
-	SIM_TEST_IS_EQUAL(strlen(test1), buff1.size());
+	SIM_ASSERT_IS_EQUAL(strlen(test1), buff1.size());
 	for (int i = 0; i < buff1.size(); ++i)
-		SIM_TEST_IS_EQUAL(test1[i], buff1[i]);
-	SIM_TEST_IS_EQUAL(3, buff1.getcount());
+		SIM_ASSERT_IS_EQUAL(test1[i], buff1[i]);
+	SIM_ASSERT_IS_EQUAL(3, buff1.getcount());
 }
 
+SIM_TEST(RefWeakObjectBuildIn)
+{
+	sim::RefObject<int> int_ptr(new int(0));
+	SIM_ASSERT_IS_TRUE(int_ptr);
+	SIM_ASSERT_IS_FALSE(NULL == int_ptr);
+	SIM_ASSERT_IS_FALSE(NULL == int_ptr.get());
+	SIM_ASSERT_IS_EQUAL(1, int_ptr.getcount());
+	SIM_ASSERT_IS_EQUAL(0, *int_ptr);
+
+	sim::RefWeakObject<int> int_weak_ptr1(int_ptr);
+	SIM_ASSERT_IS_EQUAL(1, int_weak_ptr1.getcount());
+	sim::RefWeakObject<int> int_weak_ptr2=int_ptr;
+	SIM_ASSERT_IS_EQUAL(2, int_weak_ptr2.getcount());
+
+	{
+		sim::RefObject<int> int_ptr2 = int_weak_ptr1.ref_object();
+		SIM_ASSERT_IS_TRUE(int_ptr2);
+		SIM_ASSERT_IS_FALSE(NULL == int_ptr2);
+		SIM_ASSERT_IS_FALSE(NULL == int_ptr2.get());
+		SIM_ASSERT_IS_EQUAL(2, int_ptr2.getcount());
+		SIM_ASSERT_IS_EQUAL(0, *int_ptr2);
+
+		SIM_ASSERT_IS_EQUAL(2, int_weak_ptr1.getcount());
+	}
+
+	int_ptr.reset();
+
+	{
+		sim::RefObject<int> int_ptr2 = int_weak_ptr1.ref_object();
+		SIM_ASSERT_IS_FALSE(int_ptr2);
+		SIM_ASSERT_IS_TRUE(NULL == int_ptr2);
+		SIM_ASSERT_IS_TRUE(NULL == int_ptr2.get());
+
+		SIM_ASSERT_IS_EQUAL(2, int_weak_ptr1.getcount());
+	}
+}
+
+SIM_TEST(RefWeakObjectObject)
+{
+	int i = 0;
+	{
+		sim::RefObject<RefObjectTest> object_ptr(new RefObjectTest(i));
+		sim::RefWeakObject<RefObjectTest> weak_object_ptr(object_ptr);
+		sim::RefWeakObject<RefObjectTest> weak_object_ptr2(weak_object_ptr);
+
+		object_ptr = weak_object_ptr.ref_object();
+		object_ptr = weak_object_ptr2.ref_object();
+
+		object_ptr.reset();
+
+		object_ptr = weak_object_ptr2.ref_object();
+		object_ptr = weak_object_ptr.ref_object();
+	}
+	SIM_ASSERT_IS_EQUAL(0, i);
+}
 SIM_TEST_MAIN(sim::noisy)
